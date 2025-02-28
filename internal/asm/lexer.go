6ef3f8ad7b6f4f6316603a49a20c6383e66f0e77
crookdc <e.rasmus.nilsson@icloud.com>
2025-02-28 21:27:57 +0100

    Adds case of invalid characters in lexer phase. Creates skeleton source files for upcoming implementations

--- internal/asm/lexer.go
+++ internal/asm/lexer.go
@@ -14,8 +14,8 @@ var (
 		'&':  and,
 		'|':  or,
 		';':  semicolon,
-		'(':  leftParenthesis,
-		')':  rightParenthesis,
+		'(':  lparen,
+		')':  rparen,
 	}
 	keywords = map[string]variant{
 		"JGT": jgt,
@@ -37,8 +37,8 @@ const (
 	or
 	identifier
 	integer
-	leftParenthesis
-	rightParenthesis
+	lparen
+	rparen
 	semicolon
 	jgt
 	jeq
@@ -62,6 +62,10 @@ type lexer struct {
 	cursor int
 }
 
+// next returns the next token that can be extracted from the underlying source code from the current cursor position
+// and places cursor at the next character after all characters belonging to the most recently processed token. Calling
+// next after the cursor has reached the end of the underlying source code is safe and will only result in an eof token
+// being returned together with a nil error.
 func (l *lexer) next() (token, error) {
 	if l.cursor >= len(l.src) {
 		return token{
@@ -69,8 +73,7 @@ func (l *lexer) next() (token, error) {
 		}, nil
 	}
 	char := l.src[l.cursor]
-	symbol, ok := symbols[char]
-	if ok {
+	if symbol, ok := symbols[char]; ok {
 		l.cursor++
 		return token{
 			variant: symbol,
@@ -79,6 +82,11 @@ func (l *lexer) next() (token, error) {
 	}
 	if char == '/' {
 		return l.comment()
+	}
+	if !alphanumerical(char) {
+		// At this point all the valid non-alphanumerical characters should have been processed. Therefore, if the
+		// current character is not alphanumerical it should be considered invalid.
+		return token{}, fmt.Errorf("invalid token '%s'", string(char))
 	}
 	// Identifiers cannot start with a digit, therefore we must first check if the current character is an integer to
 	// decide whether to regard this as an integer literal
