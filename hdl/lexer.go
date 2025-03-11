7b3c6e8749db7409c2688450e1a4b7e4ca8cb928
crookdc <e.rasmus.nilsson@icloud.com>
2025-03-11 19:58:02 +0100

    Adds initial implementation of HDL lexer

---
+++ hdl/lexer.go
@@ -0,0 +1,180 @@
+package hdl
+
+import (
+	"fmt"
+	"unicode"
+)
+
+var (
+	symbols = map[uint8]variant{
+		'(': leftParenthesis,
+		')': rightParenthesis,
+		'=': equals,
+		':': colon,
+		',': comma,
+		'{': leftCurlyBrace,
+		'}': rightCurlyBrace,
+		'[': leftBracket,
+		']': rightBracket,
+	}
+	keywords = map[string]variant{
+		"chip": chip,
+		"set":  set,
+		"out":  out,
+	}
+)
+
+const (
+	eof variant = iota
+	chip
+	set
+	out
+	identifier
+	integer
+	leftParenthesis
+	rightParenthesis
+	colon
+	comma
+	arrow
+	leftCurlyBrace
+	rightCurlyBrace
+	leftBracket
+	rightBracket
+	lineComment
+	leftBlockComment
+	rightBlockComment
+	equals
+)
+
+type variant int
+
+type token struct {
+	variant variant
+	literal string
+}
+
+type lexer struct {
+	src    string
+	cursor int
+}
+
+func (l *lexer) next() (token, error) {
+	l.literal(l.space)
+	if l.cursor >= len(l.src) {
+		return token{
+			variant: eof,
+		}, nil
+	}
+	char := l.src[l.cursor]
+	if symbol, ok := symbols[char]; ok {
+		l.cursor++
+		return token{
+			variant: symbol,
+			literal: string(char),
+		}, nil
+	}
+	switch char {
+	case '/':
+		if l.src[l.cursor+1] == '/' {
+			l.cursor += 2
+			return token{
+				variant: lineComment,
+				literal: "//",
+			}, nil
+		}
+		if l.src[l.cursor+1] == '*' {
+			l.cursor += 2
+			return token{
+				variant: leftBlockComment,
+				literal: "/*",
+			}, nil
+		}
+		return token{}, fmt.Errorf("invalid token '%s'", string(char))
+	case '*':
+		if l.src[l.cursor+1] == '/' {
+			l.cursor += 2
+			return token{
+				variant: rightBlockComment,
+				literal: "*/",
+			}, nil
+		}
+		return token{}, fmt.Errorf("invalid token '%s'", string(char))
+	case '-':
+		if l.src[l.cursor+1] == '>' {
+			l.cursor += 2
+			return token{
+				variant: arrow,
+				literal: "->",
+			}, nil
+		}
+		return token{}, fmt.Errorf("invalid token: '%s'", string(char))
+	}
+	// Identifiers cannot start with a digit, therefore we must first check if the current character is an integer to
+	// decide whether to regard this as an integer Literal
+	if numerical(char) {
+		return token{
+			variant: integer,
+			literal: l.literal(numerical),
+		}, nil
+	}
+	literal := l.literal(l.identifier)
+	if keyword, ok := keywords[literal]; ok {
+		return token{
+			variant: keyword,
+			literal: literal,
+		}, nil
+	}
+	return token{
+		variant: identifier,
+		literal: literal,
+	}, nil
+}
+
+// seek places the cursor at the next instance of the supplied character, skipping anything before finding a match
+func (l *lexer) seek(c uint8) error {
+	for ; l.cursor < len(l.src) && l.src[l.cursor] != c; l.cursor++ {
+	}
+	if l.cursor == len(l.src) {
+		return fmt.Errorf("character '%s' not found", string(c))
+	}
+	return nil
+}
+
+func (l *lexer) space(c uint8) bool {
+	return unicode.IsSpace(rune(c))
+}
+
+func (l *lexer) literal(fn func(uint8) bool) string {
+	literal := ""
+	for ; l.cursor < len(l.src) && fn(l.src[l.cursor]); l.cursor++ {
+		literal += string(l.src[l.cursor])
+	}
+	return literal
+}
+
+func (l *lexer) identifier(c uint8) bool {
+	if unicode.IsSpace(rune(c)) {
+		return false
+	}
+	if alphanumerical(c) {
+		return true
+	}
+	switch c {
+	case '_', '.':
+		return true
+	default:
+		return false
+	}
+}
+
+func alphanumerical(c uint8) bool {
+	return alphabetical(c) || numerical(c)
+}
+
+func alphabetical(c uint8) bool {
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+}
+
+func numerical(c uint8) bool {
+	return c >= '0' && c <= '9'
+}
---
+++ hdl/lexer_test.go
@@ -0,0 +1,161 @@
+package hdl
+
+import (
+	"testing"
+)
+
+func TestLexer_Next(t *testing.T) {
+	var tests = []struct {
+		name   string
+		src    string
+		tokens []token
+	}{
+		{
+			name: "source code for chip implementation of NOT gate",
+			src: `
+			chip not (n: 1) -> (1) {
+				out {
+					nand(n=[n, 1])
+				}
+			}
+			`,
+			tokens: []token{
+				{
+					variant: chip,
+					literal: "chip",
+				},
+				{
+					variant: identifier,
+					literal: "not",
+				},
+				{
+					variant: leftParenthesis,
+					literal: "(",
+				},
+				{
+					variant: identifier,
+					literal: "n",
+				},
+				{
+					variant: colon,
+					literal: ":",
+				},
+				{
+					variant: integer,
+					literal: "1",
+				},
+				{
+					variant: rightParenthesis,
+					literal: ")",
+				},
+				{
+					variant: arrow,
+					literal: "->",
+				},
+				{
+					variant: leftParenthesis,
+					literal: "(",
+				},
+				{
+					variant: integer,
+					literal: "1",
+				},
+				{
+					variant: rightParenthesis,
+					literal: ")",
+				},
+				{
+					variant: leftCurlyBrace,
+					literal: "{",
+				},
+				{
+					variant: out,
+					literal: "out",
+				},
+				{
+					variant: leftCurlyBrace,
+					literal: "{",
+				},
+				{
+					variant: identifier,
+					literal: "nand",
+				},
+				{
+					variant: leftParenthesis,
+					literal: "(",
+				},
+				{
+					variant: identifier,
+					literal: "n",
+				},
+				{
+					variant: equals,
+					literal: "=",
+				},
+				{
+					variant: leftBracket,
+					literal: "[",
+				},
+				{
+					variant: identifier,
+					literal: "n",
+				},
+				{
+					variant: comma,
+					literal: ",",
+				},
+				{
+					variant: integer,
+					literal: "1",
+				},
+				{
+					variant: rightBracket,
+					literal: "]",
+				},
+				{
+					variant: rightParenthesis,
+					literal: ")",
+				},
+				{
+					variant: rightCurlyBrace,
+					literal: "}",
+				},
+				{
+					variant: rightCurlyBrace,
+					literal: "}",
+				},
+				{
+					variant: eof,
+					literal: "",
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			l := lexer{src: test.src}
+			i := 0
+			for {
+				tok, err := l.next()
+				if err != nil {
+					t.Errorf("unexpected error: %v", err)
+				}
+				expected := test.tokens[i]
+				if tok.variant != expected.variant {
+					t.Errorf("expected variant '%v' but got '%v'", expected.variant, tok.variant)
+				}
+				if tok.literal != expected.literal {
+					t.Errorf("expected literal '%v' but got '%v'", expected.literal, tok.literal)
+				}
+				i++
+				if tok.variant == eof {
+					break
+				}
+			}
+			if i != len(test.tokens) {
+				t.Errorf("expected %d tokens but got %d", len(test.tokens), i)
+			}
+		})
+	}
+}
